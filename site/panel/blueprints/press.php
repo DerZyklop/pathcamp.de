4c9f5f7bf59e17b6e6a77a09760f49470d985a84
DerZyklop <mail@der-zyklop.de>
2014-02-17 22:54:11 +0100

    neues content-backup

---
+++ site/panel/blueprints/press.php
@@ -0,0 +1,24 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+# default blueprint
+
+title: Page
+pages: 
+  template: default
+files: true
+fields:
+  title: 
+    label: Title
+    type:  text
+  text: 
+    label: Text
+    type:  textarea
+    size:  large
+    buttons:
+      - h1
+      - h2
+      - h3
+      - bold
+      - italic
+      - email
+      - link
