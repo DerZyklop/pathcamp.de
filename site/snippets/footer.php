72b389f2606b068c3b49800b6037182336e268f9
DerZyklop <mail@der-zyklop.de>
2014-02-17 22:43:15 +0100

    imprint in den footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,8 @@
       <footer>
         <div class="main-width-wrap">
           <?php echo kirbytext($site->copyright()) ?>
+          <a class="right" href="/imprint">Impressum</a>
+          <div class="clearit"></div>
         </div>
       </footer>
     </section>
